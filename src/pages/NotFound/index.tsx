4851683ebf4de29c90d6cf4f83e76b49fc86c66f
Duorly <nebel.mass@gmail.com>
2023-10-19 19:22:36 +0100

    Improve design of not found page

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,7 +1,15 @@
-export const NotFound = () => {
+import { NavLink } from "react-router-dom";
+import "./style.scss";
+
+export const NotFound = (): JSX.Element => {
     return (
-        <div>
-            Non trouvé
+        <div className="notFound">
+            <div>
+                <p className="notFound__code">404</p>
+                <p className="notFound__text">Oups! La page que vous demandez n&apos;existe pas.</p>
+                <span className="home-link"><NavLink className="home-nav-link" to="/">Retourner sur la page
+                    d’accueil</NavLink></span>
+            </div>
         </div>
-    )
+    );
 }
